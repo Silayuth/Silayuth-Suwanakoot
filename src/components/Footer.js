3ab601ffd16e70a6f2c097f6103b33cd6d8eb0f5
Silayuth <rockyjimslive@gmail.com>
2024-12-15 02:36:33 +0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href=""
+                href="https://www.linkedin.com/in/silayuth-suwanakoot/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
